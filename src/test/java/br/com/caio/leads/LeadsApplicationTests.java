d67715ca0039d006ccfb38ab6e4f49f1328a3cdd
Caio Abreu <caio3015@hotmail.com>
2024-05-26 13:47:37 -0300

    config inicial projeto

---
+++ src/test/java/br/com/caio/leads/LeadsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caio.leads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
